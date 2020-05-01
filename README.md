737358e7b81b2efb495eba8abf04e77e77e68962
malpiszon <alan@malpiszon.net>
2020-05-01 19:11:16 +0200

    First version of the script

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# parganizer
+# Parkanizer API checker
 Check if there are free parking spots in OBC F parking
+
+## To send email with results
+`content=`./parkanizer.py` && [ "${content}" != "" ] && echo "${content}" | mail -s "Wolne miejsca OBC" recipient1@address.pl,recipient2@address.pl`
---
+++ parkanizer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from os import path
+import requests
+import json
+
+url = 'https://go.parkanizer.com/api/multiple-parking-lots/offers'
+spotsLeftPath = '/tmp/parkanizer.tmp'
+
+if not path.isfile(spotsLeftPath):
+    f = open(spotsLeftPath, "x")
+    f.write("0")
+    f.close()
+
+response = requests.get(url)
+parkings = json.loads(response.text)
+
+for parking in parkings['parkingLots']:
+    if parking['id'] == 'obc-plac-f':
+        f = open(spotsLeftPath, "r")
+        previousSpotsLeft = f.read()
+        f.close()
+        currentSpotsLeft = parking['startedPriceTables'][0]['spotsLeft']
+        if int(previousSpotsLeft) != int(currentSpotsLeft) and int(currentSpotsLeft) != 0:
+            f = open(spotsLeftPath, "w")
+            f.write(str(currentSpotsLeft))
+            currentPrice = parking['startedPriceTables'][0]['price']
+            print('Wolnych miejsc: ' + str(currentSpotsLeft) + ' w cenie: ' + str(currentPrice) + '. Link: https://go.parkanizer.com/#/offers')
+
+
